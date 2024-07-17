036b4d208a07783d5d7e11d3455407200d1d6775
ór23106028SavidiSithara <savidisithara@gmail.com>
2024-07-17 18:36:48 +0530

    routes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ManageEmpComponent } from './page/manage-emp/manage-emp.component';
 import { ViewEmployeeComponent } from './page/view-employee/view-employee.component';
+import { NavComponent } from './common/nav/nav.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,ManageEmpComponent,ViewEmployeeComponent],
+  imports: [RouterOutlet,ManageEmpComponent,ViewEmployeeComponent,NavComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { ManageEmpComponent } from './page/manage-emp/manage-emp.component';
+import { ViewEmployeeComponent } from './page/view-employee/view-employee.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path:"add-employee",
+        component:ManageEmpComponent
+    },
+    {
+        path:"view-all-employee",
+        component:ViewEmployeeComponent
+    }
+];
